f47f59ed643bc02d43dc52aa2331803a0cfded5f
Alan Ruelas <A01641426@tec.mx>
2023-09-26 11:29:15 -0600

    Basic floyd warshall implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <limits>
 
 using namespace std;
 
 float** createGraph(int, int);
-void fillGraph(float**&, int);
+float** createMatrix(int);
+void fillMatrix(float**&, int, int);
+void setMatrixToDefault(float**&, int);
 void printMatrix(float**, int, int);
+float** floydWarshall(float**, int);
 
 int main() {
 
@@ -18,34 +22,69 @@ int main() {
     cin >> numEdges;
 
     float** adjMatrix = createGraph(numNodes, numEdges);
+    printMatrix(adjMatrix, numNodes, numNodes);
+    float** allPairs = floydWarshall(adjMatrix, numNodes);
+    printMatrix(allPairs, numNodes, numNodes);
         
     return 0;
 }
 
 float** createGraph(int numNodes, int numEdges) {
-    float** adjMatrix = new float*[numNodes];
-    for(int i = 0; i < numNodes; i++) {
-        adjMatrix[i] = new float[numNodes];
-    }
-    fillGraph(adjMatrix, numEdges);
+    float** adjMatrix = createMatrix(numNodes);
+    fillMatrix(adjMatrix, numNodes, numEdges);
     return adjMatrix;
 }
 
-void fillGraph(float** &adjMatrix, int numEdges) {
+float** createMatrix(int numNodes) {
+    float** matrix = new float*[numNodes];
+    for(int i = 0; i < numNodes; i++) {
+        matrix[i] = new float[numNodes];
+    }
+    return matrix;
+}
+
+void fillMatrix(float** &adjMatrix, int numNodes, int numEdges) {
+    setMatrixToDefault(adjMatrix, numNodes);
     int i = 0;
     int j = 0;
     float weight = 0;
     for(int k = 0; k < numEdges; k++) {
         cin >> i >> j >> weight;
-        adjMatrix[i][j] = weight;
+        adjMatrix[i-1][j-1] = weight;
+    }
+}
+
+void setMatrixToDefault(float** &adjMatrix, int numNodes) {
+    float Inf = numeric_limits<float>::infinity();  
+    for(int i = 0; i < numNodes; i++) {
+        for(int j = 0; j < numNodes; j++) {
+            adjMatrix[i][j] = Inf;
+        }
+        adjMatrix[i][i] = 0;
     }
 }
 
 void printMatrix(float** matrix, int rows, int columns) {
+    cout << endl;
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < columns; j++) {
             cout << matrix[i][j];
         }
         cout << endl;
     }
+    cout << endl;
+}
+
+float** floydWarshall(float** weightMatrix, int numNodes) {
+    float*** D = new float**[numNodes+1];
+    D[0] = weightMatrix;
+    for(int k = 1; k < numNodes+1; k++) {
+        D[k] = createMatrix(numNodes);
+        for(int i = 0; i < numNodes; i++) {
+            for(int j = 0; j < numNodes; j++) {
+                D[k][i][j] = min(D[k-1][i][j], D[k-1][i][k] + D[k-1][k][j]);
+            }
+        }
+    }
+    return D[numNodes];
 }
