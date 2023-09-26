e5c47a28e85a505e721ce95f2d88852886125d9e
Alan Ruelas <A01641426@tec.mx>
2023-09-26 07:51:39 -0600

    Creation of a graph from input

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+float** createGraph(int, int);
+void fillGraph(float**&, int);
+void printMatrix(float**, int, int);
+
 int main() {
 
-    
+    int numNodes = 0;
+    int numEdges = 0;
+    cout << "Number of nodes: ";
+    cin >> numNodes;
+    cout << "Number of edges: ";
+    cin >> numEdges;
+
+    float** adjMatrix = createGraph(numNodes, numEdges);
+        
     return 0;
+}
+
+float** createGraph(int numNodes, int numEdges) {
+    float** adjMatrix = new float*[numNodes];
+    for(int i = 0; i < numNodes; i++) {
+        adjMatrix[i] = new float[numNodes];
+    }
+    fillGraph(adjMatrix, numEdges);
+    return adjMatrix;
+}
+
+void fillGraph(float** &adjMatrix, int numEdges) {
+    int i = 0;
+    int j = 0;
+    float weight = 0;
+    for(int k = 0; k < numEdges; k++) {
+        cin >> i >> j >> weight;
+        adjMatrix[i][j] = weight;
+    }
+}
+
+void printMatrix(float** matrix, int rows, int columns) {
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < columns; j++) {
+            cout << matrix[i][j];
+        }
+        cout << endl;
+    }
 }
