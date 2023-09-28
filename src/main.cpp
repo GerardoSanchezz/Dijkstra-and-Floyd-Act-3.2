f7570914926135f2fa0b4bbe388dbe49bdd860e2
Gerardo <gerardosanchezz14@hotmail.com>
2023-09-28 16:32:08 -0600

    Dijkstra implementation and some fixed errors in the includes

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,10 @@
 #include <limits>
 #include <vector>
 #include <queue>
-#include "Graph.h"
+#include "graph.h"
+
+// Comand to run it: g++ main.cpp Graph.cpp -o main.exe
+// ./main.exe < ../src/inputFiles/test.txt
 
 using namespace std;
 
@@ -10,6 +13,7 @@ float** createEmptyMatrix(int);
 void printMatrix(float**, int);
 float** floydWarshall(Graph);
 float** getCopyOfMatrix(float**, int);
+void dijkstra(Graph);
 
 int main() {
 
@@ -23,9 +27,12 @@ int main() {
     Graph graph = Graph(numNodes, numEdges);
     graph.fillMatrix();
 
-    printMatrix(graph.getWeightMatrix(), numNodes);
-    float** allPairs = floydWarshall(graph);
-    printMatrix(allPairs, numNodes);
+    // printMatrix(graph.getWeightMatrix(), numNodes);
+    // float** allPairs = floydWarshall(graph);
+    // printMatrix(allPairs, numNodes);
+
+    cout << "Dijkstra:" << endl;
+    dijkstra(graph);
         
     return 0;
 }
@@ -73,4 +80,40 @@ float** getCopyOfMatrix(float** matrix, int numNodes) {
         }
     }
     return copyMatrix;
+}
+
+void dijkstra(Graph graph) {
+    int n = graph.getNumNodes();
+    float** weightMatrix = graph.getWeightMatrix();
+
+    for (int source = 0; source < n; ++source) {
+        vector<float> distance(n, numeric_limits<float>::infinity());
+        vector<int> previous(n, -1);
+        priority_queue<pair<float, int>, vector<pair<float, int>>, greater<pair<float, int>>> pq;
+
+        distance[source] = 0;
+        pq.push(make_pair(0, source));
+
+        while (!pq.empty()) {
+            int u = pq.top().second;
+            pq.pop();
+
+            for (int v = 0; v < n; ++v) {
+                if (weightMatrix[u][v] > 0) {
+                    float newDistance = distance[u] + weightMatrix[u][v];
+                    if (newDistance < distance[v]) {
+                        distance[v] = newDistance;
+                        previous[v] = u;
+                        pq.push(make_pair(distance[v], v));
+                    }
+                }
+            }
+        }
+
+        for (int destination = 0; destination < n; ++destination) {
+            if (destination != source) {
+                cout << "node " << (source + 1) << " to node " << (destination + 1) << " : " << distance[destination] << endl;
+            }
+        }
+    }
 }
